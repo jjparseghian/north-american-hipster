5c815aded34306c30ca7b6dd6b5153d67356a105
Michael Angelo <yamikamisama@gmail.com>
2015-03-28 16:34:56 -0400

    bootstrap col/rows for mmessages, add icon for playing music

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -14,23 +14,13 @@ $(document).ready(function() {
 displayChatMessage(message.name, message.text);
       });
       function displayChatMessage(name, text) {
-        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('<div class="message row"/>')
+          .prepend('<em class="col-lg-8">'+text+'</em>')
+          .prepend($('<em class="col-lg-3"/>')
+          .text(name+': '))
+          .append('<i class="fa fa-music col-lg-1"></i>')
+          .appendTo($('#messagesDiv'));
         $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-        $('#messagesDiv').append('<i class="fa fa-music"></i>');
       };
-
-function singleLineSinger(){
-
-}
-
-$(i).on(click, function(event){
-  console.log(event)
-})
-
-function lineSplit(object){
-  var letters = object.message.split("")
-  return letters
-}
-
-
+      setInterval(function() {firebase.remove()}, 300000)
 });
