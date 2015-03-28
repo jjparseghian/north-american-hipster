43531455be66f59c2526eaf7cb521e1dda23cab9
James Parseghian <jjparseghian@gmail.com>
2015-03-28 17:00:16 -0400

    Add changer js

---
+++ public/js/changer.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  var color_arr = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
+'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
+'silver', 'teal', 'white', 'yellow'];
+  var chat_text = "Here is some text";
+  var text_arr = chat_text.split('');
+  console.log(text_arr);
+  $("#play").on('click', function(){
+
+  })
+
+});
