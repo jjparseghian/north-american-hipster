c5e8d5755125a60f01a68fb349035466fc5dfab1
Anna Taberski <aftaberski@gmail.com>
2015-03-28 17:54:36 -0400

    Add pastel color generator

--- public/js/changer.js
+++ public/js/changer.js
@@ -1,22 +1,13 @@
 $(document).ready(function(){
-  var color_arr = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
-  'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
-  'silver', 'teal', 'white', 'yellow'];
   var chat_text = "Here is some text";
   var text_arr = chat_text.split('');
   console.log(text_arr);
   $("#play").on('click', function(){
 
-    // function doSetTimeout(i) {
-    //   setTimeout(function() { console.log(i); }, 3000);
-    // }
-
-    // for (var i = 0; i < text_arr.length; i++) {
-    //   doSetTimeout(i);
-    // }
-
     var color_changer = function(index){
-      document.body.style.backgroundColor = color_arr[index];
+      var hue = Math.floor(Math.random() * 360);
+      var pastel = 'hsl(' + hue + ', 100%, 87.5%)';
+      document.body.style.backgroundColor = pastel;
     }
 
     var i = 0, l = text_arr.length;
