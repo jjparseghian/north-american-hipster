e5f3eccfb0fc7455cfb27c89d7fab84fc95b3364
Michael Angelo <yamikamisama@gmail.com>
2015-03-28 18:16:27 -0400

    on click of the icons consolelog a string

--- public/js/changer.js
+++ public/js/changer.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  var color_arr = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
+  'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
+  'silver', 'teal', 'white', 'yellow'];
   var chat_text = "Here is some text";
   var text_arr = chat_text.split('');
   console.log(text_arr);
