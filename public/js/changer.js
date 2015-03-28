b02df9563ff91fecce55d1032feca10bc935c3a2
Anna Taberski <aftaberski@gmail.com>
2015-03-28 19:56:10 -0400

    Add play all functionality

--- public/js/changer.js
+++ public/js/changer.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-  var color_arr = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
-  'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
-  'silver', 'teal', 'white', 'yellow'];
-  var chat_text = "Here is some text";
-  var text_arr = chat_text.split('');
-  console.log(text_arr);
+
+  // var chat_text = "Here is some text";
+  // var text_arr = chat_text.split('');
+
   $("#play").on('click', function(){
+    var message_text = $('.message.row').children('p').text();
+    var message_arr = message_text.split('');
+    console.log(message_arr);
 
     var color_changer = function(index){
       var hue = Math.floor(Math.random() * 360);
@@ -13,12 +14,13 @@ $(document).ready(function(){
       document.body.style.backgroundColor = pastel;
     }
 
-    var i = 0, l = text_arr.length;
+    var i = 0, l = message_arr.length;
       (function iterator() {
-      console.log(text_arr[i]);
+      console.log(message_arr[i]);
         color_changer(i);
+        $('#letter-view').text(message_arr[i]);
 
-      if(++i<text_arr.length) {
+      if(++i<message_arr.length) {
           setTimeout(iterator, 1000);
       }
     })();
