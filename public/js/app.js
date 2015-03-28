3c74b44a3b87fbe75989c595c838de354c0a026f
rkartzman <remykartzman@gmail.com>
2015-03-28 14:37:22 -0400

    Add function to record keypress

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
+
+var keydict = T("ndict.key");
+var midicps = T("midicps");
+T("keyboard").on("keydown", function(e) {
+  var midi = keydict.at(e.keyCode);
+  if (midi) {
+    var freq = midicps.at(midi);
+    synth.noteOnWithFreq(freq, 100);
+  }
+}).on("keyup", function(e) {
+  var midi = keydict.at(e.keyCode);
+  if (midi) {
+    synth.noteOff(midi, 100);
+  }
+}).start();
+
+
